b514e835b547f98b5c1c135d31e5c176c3b851bc
edududu <edududu@dududu.com>
2015-11-18 19:03:42 +0100

    b) part

--- Exercise 04/particlemesh.py
+++ Exercise 04/particlemesh.py
@@ -3,11 +3,12 @@ import random
 
 L = 1.0 # Length of the box
 m = 1.0 # Mass of the particle
-ngrid = 256 # Number of grids
+ngrid = 5 # Number of grids
 h = L/ngrid
 # Random position
 
-pos = (random.uniform(0, L), random.uniform(0, L))
+#pos = (random.uniform(0, L), random.uniform(0, L))
+pos=(0.5,0.5)
 
 # DENSITY FIELD
 
@@ -20,6 +21,7 @@ print pos
 
 # FALTA TENER EN CUENTA QUE OCURRE EN LOS BORDES (A QUE CELDA VA LA CONTRIBUCION? IRIA A LA SIGUIENTE,
 	#MISMO QUE DECIR QUE DE LA CAJA QUE HABRIA AL OTRO LADO ENTRARIA UNA HACIA ACA) BUSCAR MANERA INTELIGENTE DE PONER LOS IFS
+	print NO MORE SPANISH COMMENT
 for i in xrange(ngrid):
 	for j in xrange(ngrid):
 		if pos[0] - i*h < h and pos[0] - i*h > 0  and pos[1] - j*h < h and pos[1] - j*h > 0:
@@ -55,9 +57,21 @@ for i in xrange(ngrid):
 
 
 #print wp
-print rho
+#print rho
 
 rho_kspace=np.fft.fft2(rho)
+green = np.zeros((ngrid,ngrid))
 
-green=np.zeros((ngrid,ngrid))
+for i in xrange(ngrid):
+    for j in xrange(ngrid):
+        green[i][j]=1/np.sqrt(np.power(pos[0]-i,2)+np.power(pos[1]-j,2)) #green function in matrix form
+        
+print green
+        
+green_kspace=np.fft.fft2(green) #fourier transform of the green function
+phi_kspace=green_kspace*rho_kspace #convolution product
+phi_real=np.real(np.fft.ifft2(phi_kspace)) #potential
 
+print "          "
+
+print phi_real
